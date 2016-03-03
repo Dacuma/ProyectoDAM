763eb059135e6e43a7d061052ff4ffdea6d4dec3
David <David@David-PC>
2016-03-03 11:18:03 +0100

    Creada la actividad de login

--- MagicPlayers/src/dcm/proyect/magicplayers/MainActivity.java
+++ MagicPlayers/src/dcm/proyect/magicplayers/MainActivity.java
@@ -1,8 +1,10 @@
 package dcm.proyect.magicplayers;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lanzarLogin(null);
     }
 
 
@@ -18,6 +21,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void lanzarLogin(View view){
+    	Intent i = new Intent(this, Login.class);
+    	startActivity(i);
     }
     
 }
