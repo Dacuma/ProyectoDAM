e8cdd3c36f04fccdb7eaad61c83483efea048a87
SensistarX <David@David-PC>
2016-04-05 23:17:06 +0200

    Añadido método que calcula distancia entre coordenadas en Km.

--- MagicPlayers/src/dcm/proyect/magicplayers/MenuPrincipal.java
+++ MagicPlayers/src/dcm/proyect/magicplayers/MenuPrincipal.java
@@ -5,24 +5,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 //To-Do
-public class MenuPrincipal extends Activity{
+public class MenuPrincipal extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.menuprincipal);
-        //Cada vez que se accede por primera vez a la aplicación se guardan las coordenadas en la
-        //base de datos.
-        LocalizadorGPS loc = new LocalizadorGPS(this);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menuprincipal);
+		// Cada vez que se accede por primera vez a la aplicación se guardan las
+		// coordenadas en la
+		// base de datos.
+		LocalizadorGPS loc = new LocalizadorGPS(this);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 }
